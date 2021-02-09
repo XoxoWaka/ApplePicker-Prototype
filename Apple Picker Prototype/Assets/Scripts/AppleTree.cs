658355c8defa5fc3f0494dc51f445ec6a83902b8
Alex Kurochkin <pin-pong-pin@mail.ru>
2021-02-09 21:56:28 +0300

    init commit

---
+++ Apple Picker Prototype/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    private void Start()
+    {
+        //сбрасывание яблок раз в 2 секунды
+        Invoke("DropApple", 2f);
+    }
+
+    private void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    private void Update()
+    {
+        // простое перемещение
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //изменение направления
+        if (pos.x < - leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }        
+    }
+
+    private void FixedUpdate()
+    {
+        //случайное изменение направления
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
---
+++ Apple Picker Prototype/Assets/Scripts/Basket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamicallly")]
+    public Text scoreGT;
+
+    private void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
+    private void Update()
+    {
+        //получить текущие координаты указателя мыши на экране из Input
+        Vector3 mousePos2d = Input.mousePosition;
+
+        //координата Z определяет как далеко в трехмерном пространстве находится указатель мыши
+        mousePos2d.z = -Camera.main.transform.position.z;
+
+        //преобразовать точку на двумерной плоскости экрана в трехмерные координаты игры
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2d);
+
+        //переместить корзину вдоль оси X в координату X указателя мыши
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidedWith = collision.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+        int score = int.Parse(scoreGT.text);
+        score += 1;
+        scoreGT.text = score.ToString();
+
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+    }
+}
